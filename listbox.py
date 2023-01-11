0d2b29522e3749392efc89d81e3433ca876871f5
wrobs <wrs.marietta@gmail.com>
2023-01-10 22:51:23 -0500

    more listbox, no functions yet

--- listbox.py
+++ listbox.py
@@ -4,15 +4,49 @@ from tkinter import ttk
 win = Tk()
 win.title("List Box Stuff")
 
-lbox0_title = Label(win, text = "This is a listbox\n(Drag or hold control to select multiple)")
-lbox0_title.grid(row = 0, column = 0, padx = 5, pady = 5)
+win_instr = Label(win, text = "Select all that apply.\nDrag or hold CTRL to select multiple")
+win_instr.grid(row = 0, column = 0, columnspan = 5, padx = 5, pady = 5)
 
-listscroll = Scrollbar(win)
-lbox0 = Listbox(win, selectmode = "extended", yscrollcommand = listscroll.set)
-lbox0.grid(row = 1, column = 0, sticky = 'ew', padx = 5, pady = 5)
+lbox0_title = Label(win, text = "Available")
+lbox0_scroll = Scrollbar(win)
+lbox0 = Listbox(win, selectmode = "extended", yscrollcommand = lbox0_scroll.set)
+
 for i in range(25):
     lbox0.insert(END, f'Line_{i+1}')
 
-listscroll.grid(row = 1, column = 2, sticky = 'nsew', pady = 5)
-listscroll.config(command = lbox0.yview)
+lbox1_title = Label(win, text = "Selected")
+lbox1_scroll = Scrollbar(win)
+lbox1 = Listbox(win, selectmode = "extended", yscrollcommand = lbox1_scroll.set)
 
+lbox0_scroll.config(command = lbox0.yview)
+lbox1_scroll.config(command = lbox1.yview)
+
+
+b_rightall = Button(text = ">>")
+b_right = Button(text = ">")
+b_left = Button(text = "<")
+b_leftall = Button(text = "<<")
+
+b_cont = Button(text = "Continue")
+
+
+    
+    
+
+
+# place elements on window
+win_instr.grid(row = 0, column = 0, columnspan = 5, padx = 5, pady = 5)
+lbox0_title.grid(row = 1, column = 0, padx = 5, pady = 5)
+lbox0.grid(row = 2, column = 0, rowspan = 4, sticky = 'ew', padx = 5, pady = 5)
+lbox0_scroll.grid(row = 2, column = 1, rowspan = 4, sticky = 'nsw')
+
+b_rightall.grid(row = 2, column = 2, padx = 15)
+b_right.grid(row = 3, column = 2)
+b_left.grid(row = 4, column = 2)
+b_leftall.grid(row = 5, column = 2)
+
+lbox1_title.grid(row = 1, column = 3, padx = 5, pady = 5)
+lbox1.grid(row = 2, column = 3, rowspan = 4, sticky = 'ew', padx = 5, pady = 5)
+lbox1_scroll.grid(row = 2, column = 4, rowspan = 4, sticky = 'nsw')
+
+b_cont.grid(row = 6, column = 3, columnspan = 2, pady = 20, padx = 5, sticky = 'ew')
